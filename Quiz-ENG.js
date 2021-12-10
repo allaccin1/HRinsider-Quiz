790ab9c252348927c57632a1495c3d73504e7f73
allaccin1 <67004822+allaccin1@users.noreply.github.com>
2021-12-10 15:14:18 +0300

    Update Quiz-ENG.js

--- Quiz-ENG.js
+++ Quiz-ENG.js
@@ -47,11 +47,11 @@
         quizScore_text: "You answered %totalScore out of %totalQuestions questions correctly",
         quizScoreMessage_text: "",
         quizScoreRank_text: {
-          a: "Perfect, Your mark is [A]",
-          b: "You did well, Your mark is [B]",
-          c: "Not too bad, Your mark is [C]",
-          d: "We believe that you are better than getting this result, repeat the test with more focus, Your mark is [D]",
-          f: "We advise you to spend more time on learning English, Your mark is [E]"
+          a: "!Your mark is [A], Perfect",
+          b: ".Your mark is [B], You did well",
+          c: "!Your mark is [C], Not too bad",
+          d: "Your mark is [D], However, we believe that you are better than getting this result, repeat the test with more focus",
+          f: ".Your mark is [E]و We encourage you to spend more time on learning English"
         },
   
 
